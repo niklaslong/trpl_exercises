6bd70a94f9a84cd49044e4a24b61640f5910ee64
niklaslong <niklas@long.ch>
2017-12-10 10:28:43 +0100

    add variable mut example

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x = 6; //when mut keyword is not used - compiler throws "re-assignment of immutable variable" error
+    println!("The variable is {}", x);
+
+    x = 5;
+    println!("The variable is now {}", x);
 }
