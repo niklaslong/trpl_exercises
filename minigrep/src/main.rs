0d90898e920dcb04ecb70e99a9bf4f7341017587
niklaslong <niklas@long.ch>
2018-10-28 14:02:27 +0100

    add main

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,21 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file: {}", filename);
+
+    let mut f = File::open(filename).expect("file not found");
+    let mut contents = String::new();
+
+    f.read_to_string(&mut contents)
+        .expect("somthing went wrong when reading file");
+
+    println!("With text:\n{}", contents);
 }
